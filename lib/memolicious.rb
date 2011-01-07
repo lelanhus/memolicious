34c44b85ed587d0133245b88683a8f78df50a119
Leland Husband <leland.husband@gmail.com>
2011-01-06 21:25:43 -0600

    new easiness factor working

--- lib/memolicious.rb
+++ lib/memolicious.rb
@@ -1,3 +1,17 @@
+require 'date'
+
 module Memolicious
-  # Your code goes here...
+  class << self
+    
+    def next_repetition(correct)
+      Date.today
+    end
+    
+    def easiness_factor(previous_ef, quality)
+      new_ef = previous_ef - 0.8 + 0.28 * quality - 0.02 * quality * quality
+      new_ef.round(2)
+    end
+    
+    
+  end
 end
--- spec/unit/memolicious_spec.rb
+++ spec/unit/memolicious_spec.rb
@@ -1,1 +1,26 @@
-require File.expand_path(File.dirname(__FILE__) + "/../../lib/memolicious")
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Memolicious do
+    
+  context "missed question" do
+    it "should set the next repitition as today" do
+      Memolicious.next_repetition(correct = false).should == Date.today
+    end
+    
+    it "should do some more stuff so remember to implement"
+  end
+  
+  context "initial easiness factor of 2.5" do
+    it "should return a new EF of 1.96 when quality is 1" do
+      Memolicious.easiness_factor(2.5, 1).should == 1.96
+    end
+    
+    it "should return a new EF of 2.18 when quality is 2" do
+      Memolicious.easiness_factor(2.5, 2).should == 2.18
+    end
+    
+    it "should return a new EF of 1.7 when quality is 0" do
+      Memolicious.easiness_factor(2.5, 0).should == 1.7
+    end
+  end
+end
