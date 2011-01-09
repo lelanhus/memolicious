2bbe81b476f638e42563ad588691662d07e68354
Leland Husband <leland.husband@gmail.com>
2011-01-09 01:06:54 -0600

    #next_rep returns Date.today when correct: false

--- lib/memolicious.rb
+++ lib/memolicious.rb
@@ -3,5 +3,9 @@ require 'date'
 module Memolicious
   class << self
     
+    def next_rep(options = {})
+      Date.today
+    end
+    
   end
 end
--- spec/unit/memolicious_spec.rb
+++ spec/unit/memolicious_spec.rb
@@ -1,12 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Memolicious do
-
-  describe "#easiness_factor" do
-    
-  end
   
   describe "#next_rep" do
-    
+    context "next rep is today"
+      it "should schedule next rep for today if answer is incorrect" do
+        Memolicious.next_rep(correct: false).should == Date.today
+      end
+      
+      
+    end
   end
 end
