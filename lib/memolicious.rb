c1370b5698aa70c1b7127762a25d81c28ac4155a
Leland Husband <leland.husband@gmail.com>
2011-01-08 22:19:00 -0600

    starting over

--- lib/memolicious.rb
+++ lib/memolicious.rb
@@ -3,19 +3,5 @@ require 'date'
 module Memolicious
   class << self
     
-    def next_rep
-
-    end
-    
-    def easiness_factor(previous_ef, quality)
-      calculate_ef(previous_ef, quality).round(2)
-    end
-    
-    private
-    
-    def calculate_ef(previous_ef, quality)
-      previous_ef - 0.8 + 0.28 * quality - 0.02 * quality * quality
-    end
-    
   end
 end
--- spec/unit/memolicious_spec.rb
+++ spec/unit/memolicious_spec.rb
@@ -1,23 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Memolicious do
-  
-  describe "#next_repitition" do
-    
 
+  describe "#easiness_factor" do
+    
   end
   
-  context "initial easiness factor of 2.5" do
-    it "should return a new EF of 1.96 when quality is 1" do
-      Memolicious.easiness_factor(2.5, 1).should == 1.96
-    end
+  describe "#next_rep" do
     
-    it "should return a new EF of 2.18 when quality is 2" do
-      Memolicious.easiness_factor(2.5, 2).should == 2.18
-    end
-    
-    it "should return a new EF of 1.7 when quality is 0" do
-      Memolicious.easiness_factor(2.5, 0).should == 1.7
-    end
   end
 end
