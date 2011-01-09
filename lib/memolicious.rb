3b057ef6902a3db64286999264f8b85bb95af32a
Leland Husband <leland.husband@gmail.com>
2011-01-09 02:21:32 -0600

    #next_rep returns a hash of :date and :easiness_factor

--- lib/memolicious.rb
+++ lib/memolicious.rb
@@ -4,12 +4,19 @@ module Memolicious
   class << self
     
     def next_rep(options = {})
+      data = {}
+      date = calculate_date(options)
+      ef = 2.5
+      data.merge(:date => date, :easiness_factor => ef)
+    end
+    
+    private
+    
+    def calculate_date(options)
       return Date.today unless options[:correct]
       return random_date(min: 1, max: 3) if options[:iteration] == 1
       random_date(min: 3, max: 7)
     end
-    
-    private
     
     def random_date(options)
       Date.today + Random.new.rand(options[:min]..options[:max])
--- spec/unit/memolicious_spec.rb
+++ spec/unit/memolicious_spec.rb
@@ -3,27 +3,33 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Memolicious do
   
   describe "#next_rep" do
+    
+    it "should return the date of the next rep and an easiness factor" do
+      stub_next_rep(correct: true, iteration: 1).has_key?("date")
+      stub_next_rep(correct: true, iteration: 1).has_key?("easiness_factor")
+    end
+    
     it "should schedule next rep for today if answer is incorrect" do
-      stub_next_rep(correct: false).should == Date.today
+      stub_next_rep(correct: false)[:date].should == Date.today
     end
     
     context "correct answer" do
     
       it "should schedule next rep for a day in the future if answer is correct" do
-        stub_next_rep(correct: true).should > Date.today
+        stub_next_rep(correct: true)[:date].should > Date.today
       end
     
       it "should schedule next rep for one to three days if this is the first iteration" do
         three_days = Date.today + 3
-        stub_next_rep(correct: true, iteration: 1).should > Date.today
-        stub_next_rep(correct: true, iteration: 1).should < three_days
+        stub_next_rep(correct: true, iteration: 1)[:date].should > Date.today
+        stub_next_rep(correct: true, iteration: 1)[:date].should <= three_days
       end
       
       it "should schedule next rep for three to seven days if this is the second iteration" do
         three_days = Date.today + 3
         one_week = Date.today + 7
-        stub_next_rep(correct: true, iteration: 2).should > three_days
-        stub_next_rep(correct: true, iteration: 2).should < one_week
+        stub_next_rep(correct: true, iteration: 2)[:date].should >= three_days
+        stub_next_rep(correct: true, iteration: 2)[:date].should <= one_week
       end
     
     end
